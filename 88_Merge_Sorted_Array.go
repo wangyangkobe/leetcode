ef39e04b7a7e19b957572c007261cbb54c882388
Yang Wang <250801860@qq.com>
2016-12-01 16:46:39 +0800

    Update

--- 88_Merge_Sorted_Array.go
+++ 88_Merge_Sorted_Array.go
@@ -6,6 +6,26 @@
  * The number of elements initialized in nums1 and nums2 are m and n respectively.
  */
 
+// The idea is to go from the last indexes of both arrays, compare and put elements from either A or B to the final position, 
+// which can easily get since we know that A have enough space to store them all and we know size of A and B. 
+class Solution {
+public:
+    void merge(int A[], int m, int B[], int n) {
+        
+        int a=m-1;
+        int b=n-1;
+        int i=m+n-1;    // calculate the index of the last element of the merged array
+        
+        // go from the back by A and B and compare and put to the A element which is larger
+        while(a>=0 && b>=0){
+            if(A[a]>B[b])   A[i--]=A[a--];
+            else            A[i--]=B[b--];
+        }
+        
+        // if B is longer than A just copy the rest of B to A location, otherwise no need to do anything
+        while(b>=0)         A[i--]=B[b--];
+    }
+};
 func merge(nums1 []int, m int, nums2 []int, n int) {
 	res := make([]int, 0)
 	i := 0
